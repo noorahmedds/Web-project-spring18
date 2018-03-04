dff739b8be37fc9e03c1fb8219a78992af33d004
noorahmedds <noorahmedds@outlook.com>
2018-03-04 19:09:17 +0500

    First commit

---
+++ javascript/main.js
@@ -0,0 +1,41 @@
+
+// // type ahead
+// const apikey = "AIzaSyCNfTB8IMI1YgxCwUxlEBLYc59iCl21cus";
+
+// var input = "";
+// var searchInput = document.querySelector("input[type=search]");
+// var suggestions = document.querySelector(".suggestions");
+
+// console.log(searchInput);
+
+// var endpoint = "";
+
+// function makeRequest(){    
+// }
+
+
+// searchInput.addEventListener('change', makeRequest);
+
+
+// function findMatches(){
+    
+// }
+
+
+// function displayMatches(){
+//     const matchArray = findMatches(this.value, cities);
+//     const html = matchArray.map(place => {
+//       const regex = new RegExp(this.value, 'gi');
+//       const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`);
+//       return `
+//         <li>
+//           <span class="name">${cityName}</span>
+//         </li>
+//       `;
+//     }).join('');
+//     suggestions.innerHTML = html;
+// }
+
+
+// const searchInput = document.querySelector('input[type=search]');
+// const suggestions = document.querySelector('.suggestions');
